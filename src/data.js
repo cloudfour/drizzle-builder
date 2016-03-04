612131ee5333a4c79a1c976394cf7b15132e4057
Lyza Danger Gardner <lyza@lyza.com>
2016-03-04 10:03:40 -0800

    Add minor comment

--- src/data.js
+++ src/data.js
@@ -4,8 +4,8 @@ import { readFilesKeyed } from './utils';
 /**
  * Take glob in dataOpts and parse all data from matching files as YAML.
  *
- * @param {glob} dataOpts
- * @return {Promise} resolving to object of parsed data
+ * @param {glob} dataGlob
+ * @return {Promise} resolving to {Object} of keyed parsed YAML data
  */
 function prepareData (dataGlob) {
   return readFilesKeyed(dataGlob, {
