842fa760f1035f7d930ea8a90874dbf32e121e97
Lyza Danger Gardner <lyza@lyza.com>
2016-03-05 13:06:54 -0800

    Expand page-parsing functions and tests

--- src/data.js
+++ src/data.js
@@ -17,15 +17,40 @@ function prepareDataData ({data, parseFn} = {}) {
   return utils.readFilesKeyed(data, { contentFn: parseFn });
 }
 
+/**
+ * Parse data from pages and build data to be used as part of
+ * template compilation context.
+ *
+ * @param {Object} options with
+ *   - {glob} pages: glob representing where pages live
+ * @return {Promise} resolving to {Object} contextual page data
+ */
 function preparePages ({pages} = {}) {
+  // First, read pages files' content...
   return utils.readFilesKeyed(pages, {
     contentFn: (contents, path) => {
+      // Generate an object from content
       return {
         data: frontMatter(contents).attributes,
         parent: utils.parentDirname(path)
       };
     },
-    stripNumbers: false
+    stripNumbers: false // Leave leading numerals intact
+  }).then(pageFileData => {
+    // Shape page data into the objects we need
+    const pageData = {};
+    Object.keys(pageFileData).forEach(pageKey => {
+      const page = pageFileData[pageKey];
+      pageData[page.parent] = pageData[page.parent] || {
+        name: utils.titleCase(page.parent),
+        items: {}
+      };
+      pageData[page.parent].items[pageKey] = {
+        name: utils.titleCase(pageKey),
+        data: page.data
+      };
+    });
+    return pageData;
   });
 }
 
--- test/data.js
+++ test/data.js
@@ -6,14 +6,25 @@ var path  = require('path');
 
 describe ('data', () => {
   describe('parsing pages', () => {
-    it ('should correctly parse front matter', done => {
+    it ('should correctly build data object from pages', done => {
       data.preparePages({ pages: path
         .join(__dirname, 'fixtures/pages/**/*.html') })
         .then(pageData => {
-          for (var key in pageData) {
-            expect(pageData[key]).to.contain.keys('data');
-            expect(pageData[key].data).to.be.an('object');
-          }
+          expect(pageData).to.be.an('object');
+          expect(pageData).to.contain.keys('pages');
+          expect(pageData.pages).to.contain.keys('items', 'name');
+          done();
+        });
+    });
+    it ('should correctly parse front matter from pages', done => {
+      data.preparePages({ pages: path
+        .join(__dirname, 'fixtures/pages/**/*.html') })
+        .then(pageData => {
+          expect(pageData.pages.items.components)
+            .to.contain.keys('name', 'data');
+          expect(pageData.pages.items.components.data).to.be.an('Object');
+          expect(pageData.pages.items.components.data)
+            .to.contain.keys('fabricator', 'title');
           done();
         });
     });
@@ -21,7 +32,7 @@ describe ('data', () => {
       data.preparePages({ pages: path
         .join(__dirname, 'fixtures/pages/**/*.html') })
         .then(pageData => {
-          expect(pageData).to.contain.keys('04-sandbox');
+          expect(pageData.pages.items).to.contain.keys('04-sandbox');
           done();
         });
     });
