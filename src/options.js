351adf411abb9041eb920daeeafbae86729ba5a2
Lyza Danger Gardner <lyza@lyza.com>
2016-03-15 10:39:33 -0700

    Add deepmerge; remove backwards-compat for options

--- src/options.js
+++ src/options.js
@@ -1,3 +1,4 @@
+import merge from 'deepmerge';
 import Handlebars from 'handlebars';
 import yaml from 'js-yaml';
 import marked from 'marked';
@@ -19,80 +20,10 @@ const defaults = {
 };
 
 /**
- * Merge defaults into options.
- * @return {object} merged options
+ * Merge defaults into passed options
  */
-function mergeDefaults (defaultOpts, options = {}) {
-  // Please don't hate me
-  Object.keys(options).map(key => {
-    if (typeof options[key] === 'undefined') { delete options[key]; }
-    if (typeof options[key] === 'object') {
-      options[key] = mergeDefaults(defaultOpts[key], options[key]);
-    }
-  });
-  return Object.assign({}, defaultOpts, options);
+function parseOptions (options = {}) {
+  return merge(defaults, options);
 }
-
-/**
- * Map old options object shape onto new shape
- * so that we can provide backwards-compatibility with fabricator
- * The returned options {object} is of the correct shape to have
- * defaults merged into it.
- *
- * @TODO Eventually, support for fabricator/deprecated things should be
- *       moved into their own module
- * @return {object} User options
- */
-function translateOptions (options = {}) {
-
-  const {
-    data,
-    dataFn,
-    docs,
-    docsFn,
-    handlebars,
-    helpers,
-    layouts,
-    materials: newPatterns,
-    pages,
-    partials,
-    patterns,
-    views: newPages,
-    layoutIncludes: newPartials
-  } = options;
-
-  const {
-    materials: patternKey
-  } = options.keys || {};
-
-  const result = {
-    data,
-    dataFn,
-    docs,
-    docsFn,
-    handlebars,
-    helpers,
-    layouts,
-    pages: pages || newPages,
-    patterns: patterns || newPatterns,
-    partials: partials || newPartials
-  };
-
-  result.keys = {
-    patterns: patternKey
-  };
-  return result;
-}
-
-const parseOptions = options => mergeDefaults(defaults,
-  translateOptions(options));
-
-/**
- * Sigh...
- * > Single exports and multiple exports are mutually exclusive. You have to use
- * > either one the two styles. Some modules combine both styles as follows:
- * http://www.2ality.com/2015/12/babel-commonjs.html
- */
-parseOptions.translator = translateOptions;
 
 export default parseOptions;
