3a5e6a63fb54e21012e7c9b2c15bc56209fbcb60
Lyza Danger Gardner <lyza@lyza.com>
2016-03-21 16:33:27 -0700

    Extend layouts and wrap pages

--- src/render/pages.js
+++ src/render/pages.js
@@ -1,11 +1,19 @@
 import * as renderUtils from './utils';
 
+function wrapWithLayout (page, drizzleData) {
+  const layout = page.layout || 'default';
+  return `  {{#extend '${layout}' }}
+    {{#content 'body'}}${page.contents}{{/content}}
+  {{/extend}}`;
+}
+
 /**
  * Render an individual page: replace its `contents` property
  * with compiled contents
  */
 function renderPage (page, drizzleData) {
-  page.contents = renderUtils.applyTemplate(page.contents,
+  page.contents = renderUtils.applyTemplate(
+    wrapWithLayout(page, drizzleData),
     renderUtils.localContext(page, drizzleData),
     drizzleData.options);
 }
