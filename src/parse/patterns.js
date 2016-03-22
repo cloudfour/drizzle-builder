47441cd934b473f47b445abb99d6255114451fb0
Lyza Danger Gardner <lyza@lyza.com>
2016-03-22 10:28:45 -0700

    WIP: Refactoring globs, relative directory functions

--- src/parse/patterns.js
+++ src/parse/patterns.js
@@ -6,14 +6,18 @@ import * as parseUtils from './utils';
  */
 function parsePatterns (options) {
   const patternData = {};
-  return utils.readFiles(options.src.patterns, options).then(fileData => {
+  const dirPromise = utils.globRoot(options.src.patterns);
+  const readPromise = utils.readFiles(options.src.patterns, options);
+  return Promise.all([readPromise, dirPromise]).then(allData => {
+    const fileData = allData[0];
+    const globRoot = allData[1];
     fileData.forEach(patternFile => {
       const entryKey = utils.keyname(patternFile.path, { stripNumbers: false });
-      const keys     = utils.relativePathArray(patternFile.path,
+      const pathKeys = utils.relativePathArray(patternFile.path,
         options.keys.patterns);
-      const patternEntry = utils.deepObj(keys, patternData);
+      const patternEntry = utils.deepObj(pathKeys, patternData);
 
-      const idKeys = keys.map(key => utils.keyname(key));
+      const idKeys = pathKeys.map(key => utils.keyname(key));
       const pathKey = utils.keyname(patternFile.path);
       const id = idKeys.concat(pathKey).join('.');
 
--- test/parse/pages.js
+++ test/parse/pages.js
@@ -4,7 +4,7 @@ var config = require('../config');
 var expect = chai.expect;
 var parsePages = require('../../dist/parse/pages');
 
-describe ('parse/pages', () => {
+describe('parse/pages', () => {
   var pageData, opts;
   before (() => {
     opts = config.parseOptions(config.fixtureOpts);
