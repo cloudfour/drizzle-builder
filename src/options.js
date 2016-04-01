b68aac5c057c6db10ddec75d094c44415ee2da32
Lyza Danger Gardner <lyza@lyza.com>
2016-04-01 14:29:06 -0700

    Clean up, comment options parsing

--- src/options.js
+++ src/options.js
@@ -2,7 +2,9 @@ import merge from 'deepmerge';
 import defaults from './defaults';
 
 /**
- * Merge defaults into passed options
+ * Merge defaults into passed options.
+ * @param {Object} options
+ * @return {Object} merged options
  */
 function mergeOptions (options = {}) {
   return merge(defaults, options);
