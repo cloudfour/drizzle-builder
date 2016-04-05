019986185709315cd1122e311bb17f4dea02c8b5
Lyza Danger Gardner <lyza@lyza.com>
2016-04-05 11:46:52 -0700

    WIP: Rewrite error class in ES5, start tests

--- src/utils/error.js
+++ src/utils/error.js
@@ -1,32 +1,27 @@
-import log from './log';
+function DrizzleError (message, level) {
+  this.message = message;
+  this.level = level || DrizzleError.LEVELS.WARN;
+  this.stack = (new Error()).stack;
+}
 
-const ERROR_LEVELS = {
+DrizzleError.prototype = Object.create(Error.prototype);
+
+DrizzleError.prototype.handle = function (options = {}) {
+  options = Object.assign({
+    logFn: console.log,
+    throwThreshold: DrizzleError.LEVELS.ERROR
+  }, options);
+  if (this.level >= options.throwThreshold) {
+    throw this;
+  }
+  options.logFn(this.message);
+};
+
+DrizzleError.LEVELS = {
   NOTICE: 1,
   WARN: 2,
   ERROR: 3,
   FATAL: 4
 };
 
-function handleError (error, level) {
-  switch (level) {
-    case ERROR_LEVELS.NOTICE:
-      log(`NOTICE: ${error}`);
-      break;
-    case ERROR_LEVELS.WARN:
-      log(`WARN: ${error}`);
-      break;
-    case ERROR_LEVELS.ERROR:
-    default:
-      log(`ERROR: ${error}`);
-      break;
-    case ERROR_LEVELS.FATAL:
-      log(`FATAL: ${error}`);
-      process.exit(1);
-      break;
-  }
-}
-
-export {
-  handleError,
-  ERROR_LEVELS
-};
+export default DrizzleError;
--- test/utils/error.js
+++ test/utils/error.js
@@ -1,19 +1,56 @@
 /* global describe, it */
 var chai = require('chai');
-var sinon = require('sinon');
-var sinonChai = require('sinon-chai');
 var expect = chai.expect;
-chai.use(sinonChai);
-var testUtils = require('../test-utils');
+chai.use(expect);
+var DrizzleError = require('../../dist/utils/error');
+//chai.use(sinonChai);
 
-describe ('utils/error', () => {
-  describe ('low-level notices', () => {
-    it ('should log a notice when asked to', () => {
-      var logStub = sinon.stub();
-      var errorUtils = testUtils.stubbedError(logStub);
-      errorUtils.handleError('Note this', errorUtils.ERROR_LEVELS.WARN);
-      expect(logStub).to.have.been.calledOnce;
-      expect(logStub).to.have.been.calledWith('WARN: Note this');
+
+describe.only ('utils/error', () => {
+  describe ('instantiation and class', () => {
+    it ('should instantiate a subclassed Error', () => {
+      const error = new DrizzleError('random error',
+        DrizzleError.LEVELS.NOTICE);
+      expect(error).to.be.an('Object');
     });
+    it ('should maintain level information', () => {
+      const error = new DrizzleError('random error',
+        DrizzleError.LEVELS.NOTICE);
+      expect(error.level).to.equal(DrizzleError.LEVELS.NOTICE);
+    });
+    it ('should have a `message` prop', () => {
+      const error = new DrizzleError('random error',
+        DrizzleError.LEVELS.NOTICE);
+      expect(error.message).to.equal('random error');
+    });
+    it ('should define a default level if none passed', () => {
+      const error = new DrizzleError('random error');
+      expect(error.level).to.equal(DrizzleError.LEVELS.WARN);
+    });
+  });
+  describe ('handling and logging', () => {
+    describe ('throwing', () => {
+      describe ('throw threshold', () => {
+        it ('should use a default throw threshold on `handle`', () => {
+          const error = new DrizzleError('random error',
+            DrizzleError.LEVELS.ERROR);
+          expect(error.handle.bind(error)).to.throw;
+        });
+        it ('should not throw if below threshold', () => {
+          const error = new DrizzleError('random error',
+            DrizzleError.LEVELS.NOTICE);
+          expect(error.handle.bind(error)).not.to.throw;
+        });
+        it ('should take a threshold', () => {
+          const error = new DrizzleError('random error',
+            DrizzleError.LEVELS.NOTICE);
+          expect(error.handle.bind(error,
+            { throwThreshold: DrizzleError.LEVELS.NOTICE })).to.throw;
+        });
+      });
+    });
+    it ('should by default log to console on non-throw errors');
+    it ('should accept a logging function');
+    it ('should throw by default on ERROR');
   });
 });
