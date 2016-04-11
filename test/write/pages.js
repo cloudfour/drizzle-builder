3b22cef84d7b9d4a26e87f2339c97f4a9a5beca3
Lyza Danger Gardner <lyza@lyza.com>
2016-04-11 13:23:27 -0700

    Make write tests pass

--- test/write/pages.js
+++ test/write/pages.js
@@ -1,4 +1,3 @@
-/* global describe, it, before */
 var chai = require('chai');
 var expect = chai.expect;
 var config = require('../config');
@@ -6,54 +5,51 @@ var prepare = require('../../dist/prepare/');
 var parse = require('../../dist/parse/');
 var render = require('../../dist/render/');
 var writePages = require('../../dist/write/pages');
-var options = require('../../dist/init');
 var testUtils = require('../test-utils');
 
 describe ('write/pages', () => {
-  var opts = options(config.fixtureOpts);
-  var allData;
+  var drizzleData;
   describe ('write out page files', () => {
     before (() => {
-      allData = prepare(opts).then(parse).then(render).then(writePages);
-      return allData;
+      return config.init(config.fixtureOpts).then(prepare).then(parse)
+        .then(render).then(writePages).then(dData => {
+          drizzleData = dData;
+        });
     });
     it ('should write page files', () => {
-      return allData.then(drizzleData => {
-        const paths = [
-          drizzleData.pages.doThis.outputPath,
-          drizzleData.pages['04-sandbox'].outputPath,
-          drizzleData.pages['follow-me'].down.apage.outputPath,
-          drizzleData.pages.subfolder.subpage.outputPath
-        ];
-        return testUtils.areFiles(paths).then(allAreFiles => {
-          expect(allAreFiles).to.be.true;
-        });
+      const paths = [
+        drizzleData.pages.doThis.outputPath,
+        drizzleData.pages['04-sandbox'].outputPath,
+        drizzleData.pages['follow-me'].down.apage.outputPath,
+        drizzleData.pages.subfolder.subpage.outputPath
+      ];
+      return testUtils.areFiles(paths).then(allAreFiles => {
+        expect(allAreFiles).to.be.true;
       });
     });
     it ('should write page files with compiled contents', () => {
-      allData.then(drizzleData => {
-        return testUtils.fileContents(drizzleData.pages.doThis.outputPath)
-        .then(contents => {
-          expect(contents).to.contain('<h2>foobar</h2>');
-          expect(contents).to.contain('<h1>This is the Page Layout</h1>');
-          expect(contents).not.to.contain('Body content should replace this.');
-        });
+      return testUtils.fileContents(drizzleData.pages.doThis.outputPath)
+      .then(contents => {
+        expect(contents).to.contain('<h2>foobar</h2>');
+        expect(contents).to.contain('<h1>This is the Page Layout</h1>');
+        expect(contents).not.to.contain('Body content should replace this.');
       });
     });
   });
   describe ('write to page destination', () => {
     var alteredData;
     before (() => {
+      var opts = config.fixtureOpts;
       opts.dest.pages = './test/dist/pageprefixed';
-      alteredData = prepare(opts).then(parse).then(render).then(writePages);
-      return alteredData;
+      return config.init(opts).then(prepare).then(parse)
+        .then(render).then(writePages).then(aData => {
+          alteredData = aData;
+        });
     });
     it ('should use dest.pages from options', () => {
-      return alteredData.then(drizzleData => {
-        expect(drizzleData.pages.subfolder.subpage.outputPath).to.contain(
-          'pageprefixed'
-        );
-      });
+      expect(alteredData.pages.subfolder.subpage.outputPath).to.contain(
+        'pageprefixed'
+      );
     });
   });
 });
