64186fe78a3b9c293cd3bfa0b4ac6ab2b274eb95
Erik Jung <erik@cloudfour.com>
2016-05-19 14:45:03 -0700

    Attach `baseurl` property to template context

--- src/utils/context.js
+++ src/utils/context.js
@@ -1,8 +1,32 @@
 import { deepCollection } from './object'; // TODO NOPE
+import { resourcePath } from './shared';
+import path from 'path';
+
+function getBaseUrl (resource, drizzleData) {
+  const options = drizzleData.options;
+  const destRoot = options.dest.root;
+  const destResource = options.dest[
+    Object.keys(options.keys).find(
+      key => options.keys[key].singular === resource.resourceType
+    )
+  ];
+
+  const baseurl = path.relative(
+    path.dirname(resourcePath(resource.id, destResource)),
+    destRoot
+  );
+
+  return baseurl === '' ? `.${baseurl}` : baseurl;
+}
 
 function resourceContext (resource, drizzleData) {
   const context = Object.assign({}, resource);
   context.drizzle = drizzleData;
+
+  if (drizzleData.options.dest) {
+    context.baseurl = getBaseUrl(resource, drizzleData);
+  }
+
   if (typeof resource.data === 'object') {
     Object.keys(resource.data).map(dataKey =>
       context[dataKey] = resource.data[dataKey]);
