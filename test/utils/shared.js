3068735fa65448cd66a908f5a423e557a7ac6e4b
Erik Jung <erik@cloudfour.com>
2016-11-01 15:31:20 -0700

    Install and use chai-fs for better path assertions

--- test/utils/shared.js
+++ test/utils/shared.js
@@ -3,6 +3,8 @@ var chai = require('chai');
 var expect = chai.expect;
 var utils = require('../../dist/utils/shared');
 
+chai.use(require('chai-fs'));
+
 describe ('utils/shared', () => {
   describe ('idKeys', () => {
     it ('should split strings on a common character', () => {
@@ -47,15 +49,16 @@ describe ('utils/shared', () => {
   describe('resourcePath', () => {
     it ('should build a resource path from an ID', () => {
       const pathBuilt = utils.resourcePath('foo.bar.baz.ding.dong');
-      expect(pathBuilt).to.equal('bar/baz/ding/dong.html');
+      expect(pathBuilt).to.have.dirname('bar/baz/ding');
+      expect(pathBuilt).to.have.basename('dong.html');
     });
     it ('should prefix a dest path', () => {
       const pathBuilt = utils.resourcePath('foo.bar.baz.ding.dong', 'ding/wow');
-      expect(pathBuilt).to.equal('ding/wow/bar/baz/ding/dong.html');
+      expect(pathBuilt).to.have.dirname('ding/wow/bar/baz/ding');
     });
     it ('should be able to tolerate IDs that do not have separators', () => {
       const pathBuilt = utils.resourcePath('foo');
-      expect(pathBuilt).to.be.ok.and.to.equal('foo.html');
+      expect(pathBuilt).to.be.ok.and.to.have.basename('foo.html');
     });
   });
   describe('titleCase', () => {
